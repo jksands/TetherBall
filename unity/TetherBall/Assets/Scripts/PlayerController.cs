7b61481b19c7161c5d1623ff0dc4c894d3f6c70f
jxs4264 <jxs4264@rit.edu>
2020-02-19 10:49:35 -0500

    Need to clamp the velocity

--- unity/TetherBall/Assets/Scripts/PlayerController.cs
+++ unity/TetherBall/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Vector3 acceleration;
+    public Vector3 velocity;
+    public float maxSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        acceleration = Vector3.forward;
+        velocity = Vector3.zero;
+        maxSpeed = .1f;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        velocity += acceleration * Time.deltaTime;
+        Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position = transform.position + velocity;
         
     }
 }
