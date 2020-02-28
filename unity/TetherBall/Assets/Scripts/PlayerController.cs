ff87a12df1a2d67baccc8746794d0198c6e4fae3
Jonathan Sands (RIT Student) <jxs4264@ad.rit.edu>
2020-02-28 08:53:41 -0500

    MOAR level edits

--- unity/TetherBall/Assets/Scripts/PlayerController.cs
+++ unity/TetherBall/Assets/Scripts/PlayerController.cs
@@ -226,7 +226,7 @@ public class PlayerController : MonoBehaviour
             hit = new RaycastHit();
             rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
             // centralizing = true;
-            originOff = originX - transform.position.x;
+            // originOff = originX - transform.position.x;
         }
     }
     void HandleMobile()
@@ -241,7 +241,7 @@ public class PlayerController : MonoBehaviour
             hit = new RaycastHit();
             rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
             // centralizing = true;
-            originOff = originX - transform.position.x;
+            // originOff = originX - transform.position.x;
         }
     }
     void HandleRaycast(Ray ray)
